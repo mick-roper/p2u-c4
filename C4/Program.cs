c2441c8bd510c7b73984af1f6fd46557d1ca0888
Mick Roper <michael.roper@drax.com>
2018-07-09 13:27:55 +0100

    added component view

--- C4/Program.cs
+++ C4/Program.cs
@@ -2,18 +2,20 @@
 using Structurizr.Api;
 using Structurizr.Documentation;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace C4
 {
     class Program
     {
-        private const long WORKSPACE_ID = 39608;
-        private const string API_KEY = "69dd3992-503c-41c4-a655-438147ee8e07";
-        private const string API_SECRET = "e84db31a-b4d2-4405-b01d-4606a21f028f";
+        private const long WORKSPACE_ID = 39615;
+        private const string API_KEY = "1b24e557-9cec-489c-866c-776bcbf6715c";
+        private const string API_SECRET = "0c062628-2bad-429d-af16-47343e76922b";
 
         static void Main(string[] args)
         {
-            var workspace = new Workspace("P2U - Spinal Tap", "NHS data interaction system");
+            var workspace = new Workspace("P2U Spinal Tap", "NHS Integration Platform");
             var model = workspace.Model;
             model.Enterprise = new Enterprise("Pharmacy 2 U");
 
@@ -31,9 +33,12 @@ namespace C4
             #endregion
 
             #region containers
-            var p2uSpinalTapServer = p2uSpinalTap.AddContainer("Spinal Tap Server 1", "A server", "Windows Server 2016");
-            var p2uSpinalTapDbServer = p2uSpinalTap.AddContainer("Spinal Tap Database Server", "A server", "MS SQL 2017");
-            var p2uAmqpHost = p2uSpinalTap.AddContainer("AMQP host", "A server", "Windows Server 2016");
+            var p2uSpinalTapServer = p2uSpinalTap.AddContainer("Spinal Tap Server", "Server", "Windows Server 2016");
+            var p2uSpinalTapDbServer = p2uSpinalTap.AddContainer("Spinal Tap Database Server", "Server", "MS SQL 2017");
+            var p2uAmqpHost = p2uSpinalTap.AddContainer("AMQP host", "Server", "Windows Server 2016|Ubuntu 16|Debian");
+
+            p2uSpinalTapServer.Uses(p2uSpinalTapDbServer, "Gets/sets state from", "T-SQL");
+            p2uSpinalTapServer.Uses(p2uAmqpHost, "Pushes/Pull messages from", "AMQP");
             #endregion
 
             #region Components
@@ -47,23 +52,48 @@ namespace C4
 
             restClient.Uses(nhsSpine, "Makes requests to");
             restClient.Uses(messageParser, "Relays response to");
+            restClient.Uses(securityGateway, "Authenticates with");
 
             messageParser.Uses(dataSink, "processes then (optionally) routes the message");
             messageParser.Uses(restClient, "sends formatted messages to");
 
             dataSink.Uses(stateDb, "Updates");
             dataSink.Uses(p2uAmqpHost, "Routes messages to");
+
+            dataPump.Uses(stateDb, "Updates");
+            dataPump.Uses(messageParser, "Sends formatted messages to");
+            dataPump.Uses(p2uAmqpHost, "Gets messages from");
+
+            securityGateway.Uses(gemPlus, "Authenticates with");
             #endregion
 
             var systemView = workspace.Views.CreateSystemContextView(p2uSpinalTap, "System Context", "1,000 ft view");
             systemView.Add(nhsSpine);
             systemView.Add(p2uSubsystems);
             systemView.Add(gemPlus);
+            systemView.PaperSize = PaperSize.A4_Landscape;
 
             var containerView = workspace.Views.CreateContainerView(p2uSpinalTap, "Containers", "Physical layout of the solution");
             containerView.AddAllContainers();
 
+            var componentView = workspace.Views.CreateComponentView(p2uSpinalTapServer, "Spinal Tap Server Components", "The core components used to interract with NHS spine");
+            componentView.Add(p2uAmqpHost);
+            AddComponents(p2uSpinalTapServer.Components, componentView);
+            AddComponents(p2uSpinalTapDbServer.Components, componentView);
+            AddComponents(p2uAmqpHost.Components, componentView);
+            componentView.Add(gemPlus);
+            componentView.Add(nhsSpine);
+            componentView.PaperSize = PaperSize.A4_Landscape;
+
             UploadWorkspace(workspace);
+        }
+
+        private static void AddComponents(IEnumerable<Component> components, ComponentView componentView)
+        {
+            foreach (var component in components)
+            {
+                componentView.Add(component);
+            }
         }
 
         static void UploadWorkspace(Workspace workspace)
